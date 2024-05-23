9f60449de9a2eb2b61dba382805a1afeb35b543d
Muna <muai046@gmail.com>
2024-05-23 23:59:38 +0300

    Initial Commit

---
+++ MazeGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MazeGame
+//
+//  Created by Muna Aiman Al-hajj on 15/11/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MazeGame/MazeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeGameApp.swift
+//  MazeGame
+//
+//  Created by Muna Aiman Al-hajj on 15/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MazeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
